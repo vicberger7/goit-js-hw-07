dd544488a855c640e4a854f18a95ea0fef8088a3
Viktor Atsberher <143943277+vicberger7@users.noreply.github.com>
2023-12-23 20:16:46 +0100

    hw7-nlfs

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,9 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 const createButton = document.querySelector('[data-create]');
 const input = document.querySelector('input[type="number"]');
 
