fda12bda930023c14a366127689f33d689e810a0
Viktor Atsberher <143943277+vicberger7@users.noreply.github.com>
2023-12-27 11:37:24 +0100

    hw7-123nl

--- js/task-1.js
+++ js/task-1.js
@@ -10,3 +10,27 @@ items.forEach(item => {
   console.log(`Category: ${title}`);
   console.log(`Elements: ${itemsCount}`);
 });
+
+let list = [
+  'Cat',
+  'Hamster',
+  'Horse',
+  'Parrot',
+  'Bread',
+  'Parsley',
+  'Cheese',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Node.js',
+]; // ваш масив з текстами
+let lis = document.getElementsByTagName('li'); // отримуємо всі елементи <li>
+
+Array.from(lis).forEach(li => {
+  // перебираємо елементи <li>
+  if (list.includes(li.innerText)) {
+    // перевіряємо, чи містить текст з масиву
+    li.classList.add('item-unit'); // додаємо клас item-unit
+  }
+});
